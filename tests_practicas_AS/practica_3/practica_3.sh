a8dda710bacffc6c2e23c1d9929d6826c56c635e
Davidjmz12 <davidjmzom1@gmail.com>
2024-03-14 12:26:34 +0100

    a

--- tests_practicas_AS/practica_3/practica_3.sh
+++ tests_practicas_AS/practica_3/practica_3.sh
@@ -4,18 +4,57 @@
 
 
 agnadir_usuarios () {
-    echo $1
+    OLDIF="$IFS"
+    IFS=","
 
+    while read uname pswd name
+    do
+        #H
+        ( [ -z "$uname" ]  || [ -z "$pswd" ] || [ -z "$name" ] ) && echo "Campo invalido" && exit
+
+        #L,M,K
+        
+        uid_user=$(id -u "$uname" 2> /dev/null)
+        if [ $? -eq 0 ]
+        then
+            echo "El usuario $uid_user ya existe"
+        else
+            #Crear grupo si no existe
+            [ $(getent group "$uname") ] || sudo groupadd "$uname"
+
+            useradd -c "$uname" "$name" -m -g "$uname" -k UID_MIN=1815 #> /dev/null 2>&1
+            echo "$name ha sido creado"
+        fi
+
+    done < $1
+
+
+    IFS="$OLDIF"
 }
 
 borrar_usuarios () {
-    echo $1
+    OLDIF=$IFS
+    $IFS=","
+
+    #E
+    while read name
+    do
+        #H
+        [ -z $name ] && echo "Campo invalido" && exit
+    done < $1
+
+
+    $IFS="$OLDIF"
 }
 
 
+#A
 echo "Este script necesita privilegios de administracion"
 
+#D
 [ $# -ne 2 ] && echo "Numero incorrecto de parametros" && exit
+
+#C
 [ "$1" != "-a" ] && [ "$1" != "-s" ] && >&2 echo "Opcion invalida" && exit
 
 
