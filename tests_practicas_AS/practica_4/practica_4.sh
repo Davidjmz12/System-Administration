f28c4e13273951097648c478baff78cc3cf39843
CarlosGiraltF <76686462+CarlosGiraltF@users.noreply.github.com>
2024-04-16 19:43:11 +0200

    Update practica_4.sh

--- tests_practicas_AS/practica_4/practica_4.sh
+++ tests_practicas_AS/practica_4/practica_4.sh
@@ -2,19 +2,17 @@
 #825068, Jimenez, David, T, 1, A
 #831274, Giralt, Carlos, T, 1, A
 
-
-# FICHERO PARA CONEXIÓN A MÁQUINAS Y CREACIÓN/ELIMINACIÓN DE USUARIOS
-
-
-
 while read ip
 do
-    # Quiet, 1 ping, 2 seconds timeout
+    # 1 ECHO_REQUEST, 2 seconds timeout
     ping -c1 -W2 "$ip" 1> /dev/null 2>&1
     if [ $? -eq 0 ]
     then
+        # copiamos el script de añadido/borrado de usuarios y el fichero que contiene los usuarios en la 
+        # máquina remota
         scp "tests_practicas_AS/practica_4/user_management.sh" "$2" "as@$ip:~/" 1> /dev/null 2>&1
         new_dir_2="~/$(echo "$2" | grep -o "[^/]*$")"
+        # ejecutamos el script y eliminamos los ficheros previamente copiados
         ssh -n "as@$ip"  "sudo bash ~/user_management.sh $1 $new_dir_2; rm ~/user_management.sh; rm $new_dir_2" 
     else
         echo "$ip no accesible"
