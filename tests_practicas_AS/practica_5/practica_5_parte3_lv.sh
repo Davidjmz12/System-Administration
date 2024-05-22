cac1aa8be20d372fb2b2aaa5e544f43b7a19f30a
Davidjmz12 <davidjmzom1@gmail.com>
2024-05-22 12:18:21 +0200

    Creo que esta bien

--- tests_practicas_AS/practica_5/practica_5_parte3_lv.sh
+++ tests_practicas_AS/practica_5/practica_5_parte3_lv.sh
@@ -2,16 +2,7 @@
 #825068, Jimenez, David, T, 1, A
 #831274, Giralt, Carlos, T, 1, A
 
-add_partition() {
-    par="$1"
-
-    echo "Desmontando $par"
-    sudo umount "$par" > /dev/null 2>&1
-
-
-    echo "Agnadir partición al grupo"
-    sudo vgextend -f "$volume_group" "$par"  > /dev/null 2>&1
-}
+# Ejecutar como root
 
 [ $# -le 1 ] && echo "Invocar como: $0 <grupo_volumen> <particion1> <particion2> ..." && exit 1
 
@@ -19,4 +10,14 @@ volume_group="$1"
 
 shift 1 # Quitamos el argumento $1 (gropo volumen)
 
-echo "$@" | xargs -d" " -I partition add_partition "partition"
+for par in "$@"
+do
+    echo "Desmontando $par"
+    umount "$par" #> /dev/null 2>&1
+    
+    echo "Creando volumen físico"
+    pvcreate "$par" #> /dev/null 2>&1
+
+    echo "Agnadir partición al grupo"
+    vgextend -f "$volume_group" "$par"  #> /dev/null 2>&1
+done
--- tests_practicas_AS/practica_5/practica_5_parte3_vg.sh
+++ tests_practicas_AS/practica_5/practica_5_parte3_vg.sh
@@ -2,19 +2,41 @@
 #825068, Jimenez, David, T, 1, A
 #831274, Giralt, Carlos, T, 1, A
 
+# Ejecutar como root
+
 OLD_IFS="$IFS"
 IFS=","
 
 while read nGV nVL t tSF dM
 do
-    sudo vgdisplay "$nGV" >/dev/null 2>&1
+
+    device="/dev/${nGV}/${nVL}"
+    # Mirar si existe el volumen lógico
+    # lvdisplay "/dev/${nGV}/${nVL}" ??
+    # lvscan | egrep '$nVL' ??
     
     # Si existe
     if [ $? -eq 0 ]
     then
-
-    else
+        # Extender el volumen lógico
+        lvextend -L $t $device
+    else # SI no existe
         
+        # Creamos el volúmen lógico
+        lvcreate -L "$t" --name "$nVL" "$nGV" 
+
+        # Le damos formato
+        mkfs -t "$tSF" "$device"
+
+        # Montamos
+        mount -t "$tSF" "$device"
+
+        # Buscamos para añadirlo en montado automático
+        UUID_device=$(lsblg -o NAME,UUID | egrep '$nVL' | awk '{print $2}')
+
+        echo "UUID=$UUID_device $dM $tSF errors=remount-ro 0 2" > /etc/fstab 
+
+
     fi
 
 done < &1
